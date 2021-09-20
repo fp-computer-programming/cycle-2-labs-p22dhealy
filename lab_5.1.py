c18085cee075133228d281c9b2b1aede93840388
p22dhealy <p22dhealy@fairfieldprep.org>
2021-09-20 10:04:21 -0400

    Submit Cycle 2 Lab 5

---
+++ lab_5.1.py
@@ -0,0 +1,24 @@
+#author: DMH (AMDG) 9/20/21
+
+
+
+# Question 1
+x = x + 5 
+
+# Question 2
+y -= 3
+
+# Question 3
+z = z ** 2
+
+# Question 4
+w %= 2
+
+# Question 5
+t /= 5
+
+# Question 6 
+a = a * 12
+
+# Question 7 
+i //= 7 
