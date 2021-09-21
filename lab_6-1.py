e6ea91c75a665485f4296ea47348c57ec70372c7
p22dhealy <p22dhealy@fairfieldprep.org>
2021-09-21 09:51:51 -0400

    Submit Cycle 2 Lab 5

---
+++ lab_6-1.py
@@ -0,0 +1,12 @@
+#author: DMH (AMDG) 9/21/21
+
+#First Name
+name = input("What Is Your First Name?")
+
+#Last Name
+name2= input ("What Is Your Last Name?")
+
+#Full Name 
+Full_Name = name + name2 
+print(Full_Name)
+
